effc0f0cf0f4bd51e621c9e27fb570a6f728cdef
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-12-04 16:22:18 -0500

    feat: implement caesar pseudo
    only prints output for now

--- src/caesar.ts
+++ src/caesar.ts
@@ -1,14 +1,57 @@
+import * as fs from 'fs/promises';
 import { nlEcho } from './utils';
+import path from 'path';
 
 (async () => {
-	// Check for samples dir for text files
-	// Check if output dir exists
-	// Create timestamped dir for per-file output
+	const dirPath = path.join(process.cwd(), 'samples', 'caesar');
+	const files = await fs.readdir(dirPath);
+	if (files.length === 0) {
+		nlEcho(`no files to parse in ${dirPath}`);
+		return;
+	}
 	// For each text file
-	// Ingest as string
-	// Split input string
-	// 2d array with 25 elements, per letter
-	// Per character swap based on if [A-Za-z] charcode (utf-8 code?), 25 times
-	// .join() all into string
-	// Print 2d array to output file with same name
+	for (const file of files) {
+		// Ingest as string
+		const inputI = await fs.readFile(path.join(dirPath, file), {
+			encoding: 'utf-8',
+		});
+		const input = inputI.toLowerCase();
+
+		nlEcho(`~~~ File:${file}\n---\n${input.slice(0, 40)}...\n`);
+
+		const iteratedStringOutput: string[] = [];
+		for (let i = 1; i < 26; i++) {
+			// Per character swap based on if [A-Za-z] charcode, 25 times
+			const bumpOneCharOutput: string[] = [];
+			for (let j = 0; j < input.length; j++) {
+				if (/[A-Z]/.test(input[j])) {
+					const newCharCode = input[j].charCodeAt(0) + i;
+					bumpOneCharOutput.push(
+						String.fromCharCode(
+							newCharCode > 90 ? 64 + newCharCode - 90 : newCharCode,
+						),
+					);
+				} else if (/[a-z]/.test(input[j])) {
+					const newCharCode = input[j].charCodeAt(0) + i;
+					bumpOneCharOutput.push(
+						String.fromCharCode(
+							newCharCode > 122 ? 96 + newCharCode - 122 : newCharCode,
+						),
+					);
+				} else {
+					// console.log(input[j]);
+					bumpOneCharOutput.push(input[j]);
+				}
+			}
+			console.log(bumpOneCharOutput.join(''));
+			iteratedStringOutput.push(bumpOneCharOutput.join(''));
+		}
+		// nlEcho(
+		// 	`Output:\n---\n${iteratedStringOutput.map((str) => `${str.slice(0, 40)}...`).join('\n')}\n~~~`,
+		// );
+		// .join() all into string
+		// Check if output dir exists
+		// Create timestamped dir for per-file output
+		// Print 2d array to output file with same name
+	}
 })();
