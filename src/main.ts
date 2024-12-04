ec3f97a34243cef9290bf81d595eea346934b0ea
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-12-04 00:33:56 -0500

    chore: layout pseudocode

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
 const nlEcho = (line: string) => console.log(`\n${line}`);
+
+(async () => {
+	// Check for samples dir for text files
+	// Check if output dir exists
+	// Create timestamped dir for per-file output
+	// For each text file
+	// Ingest as string
+	// Split input string
+	// 2d array with 25 elements, per letter
+	// Per character swap based on if [A-Za-z] charcode (utf-8 code?), 25 times
+	// .join() all into string
+	// Print 2d array to output file with same name
+})();
